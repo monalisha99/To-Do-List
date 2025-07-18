b6e5fbbeb89ce07bc128e68da7053cd72bffc520
monalisha99 <bmobalisha1@gmail.com>
2025-07-18 23:15:36 +0530

    widgets

--- todo.py
+++ todo.py
@@ -1,67 +1,28 @@
-# import tkinter library,using alias tk
 import tkinter as tk
+import tkinter.font as font
 
-# initiate main window using Tk() method
+def on_select():
+    print('Variable selected.', var.get())
+
+
 root = tk.Tk()
 
-# define window size
-root.geometry("640x480") 
+root.title('To Do List')
+root.geometry('640x480')
 
-# define window title
-root.title("First GUI by Monalisha")
-
-# add a label
-label = tk.Label(root, text="Hello world!", font=('Calibri', 18))
-label.pack(padx=20, pady=20)
-label1 = tk.Label(root,text="This is my first GUI",font=('Arial', 16))
-label1.pack()
-label3 = tk.Label(root, text="I am starting from the basics of tkinter", font=('Arial', 16))
-label3.pack()
-
-# generate text box
-textbox = tk.Text(root, height=4, font=('Arial', 16))
-textbox.pack(padx=10, pady=10)
-
-# generate the button
-button = tk.Button(root, text="Click Here!", font=('Arial', 18))
-button.pack(padx=10, pady=10)
-
-buttonframe = tk.Frame(root)
-buttonframe.columnconfigure(0, weight=1)
-buttonframe.columnconfigure(1, weight=1)
-buttonframe.columnconfigure(2, weight=1)
-
-btn1 = tk.Button(buttonframe, text="1", font=("Arial", 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+# variables
+var = tk.StringVar(value="A")
 
 
-btn2 = tk.Button(buttonframe, text="2", font=("Arial", 18))
-btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+# Add Widgets here, we will use .pack() method
 
-btn3 = tk.Button(buttonframe, text="3", font=("Arial", 18))
-btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
+entry_box_label = tk.Label(root, text="Enter Task: ", font=("courier", 18))
+entry_box_label.grid(row=0, column=0)
 
-btn4 = tk.Button(buttonframe, text="4", font=("Arial", 18))
-btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
+entry_box = tk.Entry(root, width=60)
+entry_box.grid(row=0, column=1)
 
-btn5 = tk.Button(buttonframe, text="5", font=("Arial", 18))
-btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
+radio_btn = tk.Radiobutton(root, padx=20, variable=var, value='A, command=on_select')
+radio_btn.grid(row=0, column=2)
 
-btn6 = tk.Button(buttonframe, text="6", font=("Arial", 18))
-btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
-
-buttonframe.pack(fill='x')
-
-#
-anotherbutton = tk.Button(root, text="TEST")
-anotherbutton.place(x= 150, y=150, height=50, width=50)
-
-
-
-# loop the window across the screen
 root.mainloop()
-
-
-
-
-
